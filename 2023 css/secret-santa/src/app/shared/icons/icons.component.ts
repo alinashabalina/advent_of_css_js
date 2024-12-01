f5e665291f3eaee24272e528c69739303adc097c
Alina Shabalina <alinavshabalina@gmail.com>
2024-12-02 00:15:51 +0100

    Project setup

--- 2023 css/secret-santa/src/app/shared/icons/icons.component.ts
+++ 2023 css/secret-santa/src/app/shared/icons/icons.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Input} from '@angular/core';
 
 @Component({
   selector: 'app-icons',
@@ -8,5 +9,5 @@ import { Component } from '@angular/core';
   styleUrl: './icons.component.css'
 })
 export class IconsComponent {
-
+  @Input() svgId:string = ''
 }
