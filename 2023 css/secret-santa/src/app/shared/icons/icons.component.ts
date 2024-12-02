e501d86f52cc3aba33db9fcfc5e22ab5f05d01cb
Alina Shabalina <alinavshabalina@gmail.com>
2024-12-02 10:31:31 +0100

    Fix href props

--- 2023 css/secret-santa/src/app/shared/icons/icons.component.ts
+++ 2023 css/secret-santa/src/app/shared/icons/icons.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { Input} from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-icons',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './icons.component.html',
   standalone: true,
   styleUrl: './icons.component.css'
 })
 export class IconsComponent {
-  @Input() svgId:string = ''
+  @Input() svgId:string = ""
 }
