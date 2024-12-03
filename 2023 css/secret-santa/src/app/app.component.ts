164e3fc928e53eab2e45740837e8e688d53d9181
Alina Shabalina <alinavshabalina@gmail.com>
2024-12-03 23:21:57 +0100

    Fix placeholder bug

--- 2023 css/secret-santa/src/app/app.component.ts
+++ 2023 css/secret-santa/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import {IconsComponent} from './shared/icons/icons.component';
 import {AvatarComponent} from './shared/avatar/avatar.component';
 import {InputFieldComponent} from './shared/input-field/input-field.component';
+import {InputEmailComponent} from './shared/input-email/input-email.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, IconsComponent, AvatarComponent, InputFieldComponent],
+  imports: [RouterOutlet, IconsComponent, AvatarComponent, InputFieldComponent, InputEmailComponent],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.css'
---
+++ 2023 css/secret-santa/src/app/shared/input-email/input-email.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input-email',
+  imports: [],
+  templateUrl: './input-email.component.html',
+  standalone: true,
+  styleUrl: './input-email.component.css'
+})
+export class InputEmailComponent {
+
+}
